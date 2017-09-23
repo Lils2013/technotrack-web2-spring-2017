3e7e45048493ed4ee37951cbaa621eb5ad892d9d
Alexander Tsoy <Lils2010@gmail.com>
2017-09-23 15:33:29 +0300

    fixed a lot of SHIT

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
-
+from django.utils.deconstruct import deconstructible
 
 class User(AbstractUser):
     pass
@@ -19,21 +19,7 @@ class ModelWithDates(models.Model):
 
 
 class ModelWithAuthor(models.Model):
-    author = models.ForeignKey(User)
-
-    class Meta:
-        abstract = True
-
-
-class Like(ModelWithDates, ModelWithAuthor):
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    object = GenericForeignKey('content_type', 'object_id')
-
-
-class LikeAble(models.Model):
-    likes = GenericRelation(Like, object_id_field='object_id', content_type_field='content_type')
-    likes_count = models.IntegerField(default=0)
+    author = models.ForeignKey(User,default=1)
 
     class Meta:
         abstract = True
@@ -47,11 +33,40 @@ class WatchableModel(models.Model):
         abstract = True
 
 
-class Comment(ModelWithAuthor, ModelWithDates, LikeAble):
+class Like(ModelWithDates, ModelWithAuthor, WatchableModel):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    object = GenericForeignKey('content_type', 'object_id')
+
+    def __unicode__(self):
+        return u'Like ' + str(self.pk) + u' by ' + self.author.username
+
+    def get_title_for_event(self, eventtype):
+        return str(eventtype.content_type) + u' ' \
+               + str(eventtype.object_id) + u' was liked by ' + str(eventtype.author.username)
+
+
+class LikeAble(models.Model):
+    likes = GenericRelation(Like, object_id_field='object_id', content_type_field='content_type')
+    likes_count = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class Comment(ModelWithAuthor, ModelWithDates, LikeAble, WatchableModel):
     text = models.CharField(max_length=255)
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     object = GenericForeignKey('content_type', 'object_id')
+
+    def __unicode__(self):
+        return u'Comment ' + str(self.pk) + u' by ' + self.author.username
+
+    def get_title_for_event(self, eventtype):
+        return u'comment ' + str(eventtype.text) + u' was posted on ' +\
+               str(eventtype.content_type) + u' ' \
+               + str(eventtype.object_id) + u' by ' + str(eventtype.author.username)
 
 
 class CommentAble(models.Model):
@@ -65,16 +80,24 @@ class CommentAble(models.Model):
 class Post(ModelWithAuthor, ModelWithDates, LikeAble, CommentAble, WatchableModel):
     text = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return u'Post ' + str(self.pk) + u' by ' + self.author.username
+
     def get_title_for_event(self, eventtype):
-        return eventtype.text
+        return u'post ' + str(eventtype.text) + u' was posted by ' + str(eventtype.author.username)
 
 
-class Subscription(ModelWithDates):
-    source = models.ForeignKey(User, related_name='source')
+class Subscription(ModelWithAuthor, ModelWithDates, WatchableModel):
     target = models.ForeignKey(User, related_name='target')
 
+    def __unicode__(self):
+        return u'Subscription ' + str(self.pk) + u', source: ' + self.author.username + u', target: ' + self.target.username
+
+    def get_title_for_event(self, eventtype):
+        return str(eventtype.author.username) + u' has subscribed to  ' + str(eventtype.target.username)
+
     class Meta:
-        unique_together = ('source', 'target',)
+        unique_together = ('author', 'target',)
 
 
 class Event(ModelWithAuthor, ModelWithDates):
@@ -82,3 +105,6 @@ class Event(ModelWithAuthor, ModelWithDates):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     object = GenericForeignKey('content_type', 'object_id')
+
+    def __unicode__(self):
+        return u'Event ' + str(self.pk) + u', title: ' + str(self.title)
